5f3d238d4af082862cabab3c839beb2177846948
Rankhole <muhammed_ali96@web.de>
2021-01-22 11:28:36 +0100

    Try to fix tests

--- songsWS/src/main/java/htwb/ai/ALIS/Application.java
+++ songsWS/src/main/java/htwb/ai/ALIS/Application.java
@@ -3,8 +3,11 @@ package htwb.ai.ALIS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("htwb.ai.ALIS.repository")
 class Application {
     public static void main(String[] args) {
         System.setProperty("server.servlet.context-path", "/songsWS-ALIS");
